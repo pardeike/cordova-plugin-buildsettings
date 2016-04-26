8c185993ea84ccc8f098fc383f3cea8703932c92
Andreas Pardeike <andreas@pardeike.net>
2016-04-26 10:20:51 +0200

    wrong function name

--- www/buildsettings.js
+++ www/buildsettings.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-    buildsettings: function (key, defaultValue, successCallback, errorCallback) {
+    get: function (key, defaultValue, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "BuildSettings", "getValue", [key, defaultValue]);
     }
 
